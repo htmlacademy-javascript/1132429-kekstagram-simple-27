d9c049c71cfc9f6a5c192e24a0268e5fa44714fd
Kirill Soldatov <k.igorevitch88@yandex.ru>
2022-10-13 22:06:45 +0300

    Создание массива объектов

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+const PICTURES_COUNT = 25;
+
+const likesCount = {
+  MIN: 15,
+  MAX: 200,
+};
+
+const сommentsCount = {
+  MIN: 0,
+  MAX: 200,
+};
+
+const PICTURES_DESCRIPTION = [
+  'Вау!',
+  'Круто!',
+  'Не очень.',
+  'Могло бы быть и лучше..',
+  'Хорошая фотография!',
+  'Хорошего отдыха!'
+];
+
 /*
 Функция, возвращающая случайное целое число из переданного диапазона включительно:
 1. Объявляет функцию с двумя параметрами: минимальным и максимальным значением;
@@ -6,18 +27,15 @@
 [2] - умножение разницы заданных параметров на рандомное число, [3] - прибавляет минимальное число, чтобы полученное число находилось в диапазоне заданных параметров;
 4. Вызывает функцию с заданными параметрами;
 */
+
 function getRandomNumber (minNum, maxNum) {
   if (minNum < 0 || maxNum < minNum) {
     return NaN;
   }
-  const lower = Math.ceil(Math.min(minNum, maxNum));
-  const upper = Math.floor(Math.max(minNum, maxNum));
-
-  const result = (upper - lower + 1) * Math.random + lower;
-  return result;
+  return Math.round((maxNum - minNum) * Math.random() + minNum);
 }
 
-getRandomNumber(0, 5);
+getRandomNumber();
 
 /*
 Функция для проверки максимальной длины строки:
@@ -25,8 +43,33 @@ getRandomNumber(0, 5);
 2. Возвращает булевое значение 'true' в случае если длина строки меньшь либо равна максимальной длине строки
 3. Вызывает функцию с заданными параметрами;
 */
-function checkStringLength (str, maxLength) {
-  return str.length <= maxLength;
+
+function checkStringLength (string, maxLength) {
+  return string.length <= maxLength;
 }
 
 checkStringLength('', 140);
+
+/* Функция, которая возвращает случайный элемент массива */
+
+const getRandomArrayElement = (array) =>
+  array[getRandomNumber(0, array.length - 1)];
+
+/*Создание массива объектов*/
+
+const createPicturesDescription = (count) => {
+  const photosList = [];
+
+  for (let i = 1; i <= count; i++) {
+    photosList[i - 1] = {
+      id: i,
+      url: `photos/${i}.jpg`,
+      description: getRandomArrayElement(PICTURES_DESCRIPTION),
+      likes: getRandomNumber(likesCount.MIN, likesCount.MAX),
+      comments: getRandomNumber(сommentsCount.MIN, сommentsCount.MAX),
+    };
+  }
+  return photosList;
+};
+
+createPicturesDescription(PICTURES_COUNT);
