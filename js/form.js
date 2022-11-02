b59b5fc528fec47bae96080e9d8f3b8f697d755d
Kirill Soldatov <k.igorevitch88@yandex.ru>
2022-11-02 21:15:29 +0300

    Добавляет проверку валидации

--- js/form.js
+++ js/form.js
@@ -1,33 +1,45 @@
-import {isEnterKey, isEscapeKey} from './mock/util.js';
+import {isEnterKey, isEscapeKey} from './util.js';
+
+const form = document.querySelector('.img-upload__form');
+const formOpenElement = document.querySelector('#upload-file');
+const formCloseElement = form.querySelector('#upload-cancel');
+const editorForm = form.querySelector('.img-upload__overlay');
+
+const CommentsLength = {
+  MIN: 20,
+  MAX: 140,
+};
+
+const onFormEscKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeEditorForm();
+  }
+};
+
+const openEditorForm = () => {
+  editorForm.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+
+  document.addEventListener('keydown', onFormEscKeydown);
+};
+
+function closeEditorForm() {
+  editorForm.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+
+  document.removeEventListener('keydown', onFormEscKeydown);
+  formOpenElement.value = '';
+}
+
+/**
+* Функция по проверке длины комментария
+* @param {string} comment - принимает комментарий
+* @return {boolean} - вовзращает булевое значение
+*/
+const isCommentsLength = (comment) => comment.length >= CommentsLength.MIN && comment.length <= CommentsLength.MAX;
 
 const addEventListenersToForm = () => {
-  const form = document.querySelector('.img-upload__form');
-  const formOpenElement = document.querySelector('#upload-file');
-  const formCloseElement = form.querySelector('#upload-cancel');
-  const editorForm = form.querySelector('.img-upload__overlay');
-
-  const onFormEscKeydown = (evt) => {
-    if (isEscapeKey(evt)) {
-      evt.preventDefault();
-      closeEditorForm();
-    }
-  };
-
-  const openEditorForm = () => {
-    editorForm.classList.remove('hidden');
-    document.body.classList.add('modal-open');
-
-    document.addEventListener('keydown', onFormEscKeydown);
-  };
-
-  function closeEditorForm() {
-    editorForm.classList.add('hidden');
-    document.body.classList.remove('modal-open');
-
-    document.removeEventListener('keydown', onFormEscKeydown);
-    formOpenElement.value = '';
-  }
-
   formOpenElement.addEventListener('click', () => {
     openEditorForm();
   });
@@ -47,10 +59,23 @@ const addEventListenersToForm = () => {
       closeEditorForm();
     }
   });
+
+  const pristine = new Pristine(form, {
+    classTo: 'img-upload__text',
+    errorTextParent: 'img-upload__text',
+  });
+
+  form.addEventListener('submit', (evt) => {
+    if (!pristine.validate()) {
+      evt.preventDefault();
+    }
+  });
+
+  pristine.addValidator(
+    form.querySelector('.text__description'),
+    isCommentsLength,
+    `Необходимое количество символов от ${CommentsLength.MIN} до ${CommentsLength.MAX}`
+  );
 };
-// const pristine = new Pristine(form, {
-//   classTo: 'img-upload__text',
-//   errorTextParent: 'img-upload__text',
-// });
 
 export {addEventListenersToForm};
